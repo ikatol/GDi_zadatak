37e86df1a31cf93ed118afa7de191508303863f5
ikatoliko <37219993+ikatoliko@users.noreply.github.com>
2023-08-19 18:17:29 +0200

    Added the new solution to the new hierarchy.

---
+++ MVC/GDi_zadatak_MVC_FE/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace GDi_zadatak_MVC_FE.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
